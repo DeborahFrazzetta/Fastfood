73cbd895706097462c875b20d9837acbd1509c5c
Deborah Frazzetta <deborah.frazzetta@pegaso2000.it>
2023-06-21 12:34:58 +0200

    testo del messaggio

---
+++ FastFood/Model/Cliente.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FastFood.Model
+{
+    [Table("cliente")]
+    public class Cliente
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(45)]
+        public string nome { get; set; }
+        [Required]
+        public bool is_vegetariano { get; set; }
+        [Required]
+        public int eta { get; set; }    
+
+
+
+    }
+}
---
+++ FastFood/Model/Piatto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FastFood.Model
+{
+    [Table("piatto")]
+    public class Piatto
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        [MaxLength(45)]
+        public string descrizione { get; set; }
+        [Required]
+        public bool is_vegetariano { get; set; }
+        [Required]
+        public int prezzo { get; set; }
+
+
+
+    }
+}
