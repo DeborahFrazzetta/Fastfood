19367c454617f0bb5129c7cbc3e67a1f270d1483
Deborah Frazzetta <deborah.frazzetta@pegaso2000.it>
2023-06-22 11:32:59 +0200

    Nuovo branch develop, creo dto e repo

---
+++ FastFood/DTO/ClienteDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace FastFood.DTO
+{
+    public class ClienteDTO
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public bool is_vegetariano { get; set; }
+        public int eta { get; set; }
+
+
+    }
+}
---
+++ FastFood/DTO/PiattoDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace FastFood.DTO
+{
+    public class PiattoDTO
+    {
+        public int id {  get; set; }
+        public string descrizione { get; set; }
+        public bool is_vegetariano { get; set; }
+        public int prezzo { get; set; }
+
+       
+    }
+}
