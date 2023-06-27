5600e157d2b66a307e29b4624f5d791ba87f8a8f
Deborah Frazzetta <deborah.frazzetta@pegaso2000.it>
2023-06-27 11:50:11 +0200

    controller cliente

--- FastFood/Controllers/ClienteController.cs
+++ FastFood/Controllers/ClienteController.cs
@@ -1,10 +1,12 @@
-﻿using FastFood.Repository;
+﻿using FastFood.DTO;
+using FastFood.Model;
+using FastFood.Repository;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FastFood.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     [ApiController]
     public class ClienteController : ControllerBase
     {
@@ -14,5 +16,70 @@ namespace FastFood.Controllers
         {
             _repo = repo;
         }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Cliente> list = _repo.GetAll();
+            List<ClienteDTO> result = list.Select(x => entityToDto(x)).ToList();
+            return this.Ok(result);
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetById(int id)
+        {
+            Cliente c = _repo.GetById(id);
+            if (c == null) return this.NotFound();
+
+
+            ClienteDTO result = entityToDto(c);
+            return this.Ok(result); 
+        }
+
+        [HttpPost]
+        public IActionResult Save([FromBody] ClienteDTO dto)
+        {
+            Cliente saving = dtoToEntity(dto); 
+            var result = _repo.Save(saving);
+            return this.Ok(result);
+
+        }
+
+        private ClienteDTO entityToDto(Cliente c)
+        {
+            ClienteDTO dto = new ClienteDTO();
+           dto.id = c.id;
+           dto.nome = c.nome;
+            dto.is_vegetariano = c.is_vegetariano;
+            dto.eta = c.eta;
+            return dto;
+        }
+
+        private Cliente dtoToEntity(ClienteDTO dto)
+        {
+            Cliente c = new Cliente();
+            c.id = dto.id;
+            c.nome = dto.nome;
+            c.is_vegetariano = dto.is_vegetariano;
+            c.eta = dto.eta;
+
+            return c;
+        }
+
+        
+            
+
+            //var result = new List<ClienteDTO>();
+            //var entities = _repo.GetAll();
+            //foreach (var entity in entities)
+            //{
+            //    result.Add(ClienteDTO.CreateDTO(entity));
+            //}
+            //return this.Ok(result);
+        }
+
+
+        
     }
-}
+
--- FastFood/DTO/ClienteDTO.cs
+++ FastFood/DTO/ClienteDTO.cs
@@ -1,4 +1,6 @@
-﻿namespace FastFood.DTO
+﻿using FastFood.Model;
+
+namespace FastFood.DTO
 {
 
     public class ClienteDTO
@@ -6,9 +8,32 @@
         public int id { get; set; }
         public string nome { get; set; }
         public bool is_vegetariano { get; set; }
-        public  int eta { get; set; }
+        public  int eta { get; set; }            
+        public ClienteDTO() { }
 
+        public static ClienteDTO CreateDTO (ClienteDTO entity)
+        {
+            ClienteDTO dto = new ClienteDTO
+            {
+                id = entity.id,
+                nome = entity.nome,
+                is_vegetariano = entity.is_vegetariano,
+                eta = entity.eta,
+            };
+            return dto;
 
+        }
+
+        public static Cliente GetEntity(ClienteDTO dto)
+        {
+            return new Cliente
+            {
+                id = dto.id,
+                nome = dto.nome,
+                is_vegetariano = dto.is_vegetariano,
+                eta=dto.eta,
+            }
+        }
 
     }
 }
